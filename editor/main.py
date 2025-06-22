794d36ef7aff3ced73f9a6b54ee1f44ee83c5d3b
mgismissing <gabrycraft2010@gmail.com>
2025-06-23 00:55:05 +0200

    Update Phosphor.py API

--- editor/main.py
+++ editor/main.py
@@ -4,4 +4,4 @@ overlay = ph.Overlay("com.android.systemui", "PhosphorSystemUI", "com.morph3us.p
 
 overlay.link_resource(ph.ResourceTarget("string", "sec_quick_settings_flashlight_label"), ph.Resource("string", "qs_flashlight", "Flashbang"))
 
-print(overlay.overlays)
+overlay.build_linux()
--- editor/phosphor.py
+++ editor/phosphor.py
@@ -1,33 +1,40 @@
 from typing import Any, overload
+import shutil
+import os
 
 class ResourceTarget:
     @overload
     def __init__(self, type_: str, name: str): ...
     @overload
     def __init__(self, resid: str): ...
-    def __init__(self, **kwargs: str):
-        # TODO: Implement __init__
-        self.type_ = resid.split("/", 1)[0]
-        self.name = resid.split("/", 1)[1]
+    def __init__(self, *args: str):
+        if len(args) == 1:
+            self.type_ = args[0].split("/", 1)[0]
+            self.name = argd[1].split("/", 1)[1]
+        else:
+            self.type_ = args[0]
+            self.name = args[1]
     def __str__(self):
-        return f"@{self.type_}/{self.name}"
+        return f"{self.type_}/{self.name}"
     def __repr__(self):
-        return f"ResourceTarget(resid={self.__str__()})"
+        return f"ResourceTarget(\"{self.__str__()}\")"
 
 class Resource(ResourceTarget):
     @overload
-    def __init__(self, type_: str, name: str, value: Any):
-        super().__init__(type_, name)
-        self.value = value
+    def __init__(self, type_: str, name: str, value: Any): ...
     @overload
-    def __init__(self, resid: str, value: Any):
-        self.type_ = resid.split("/", 1)[0]
-        self.name = resid.split("/", 1)[1]
-        self.value = value
+    def __init__(self, resid: str, value: Any): ...
+    def __init__(self, *args):
+        if len(args) == 2:
+            super().__init__(args[0])
+            self.value = args[1]
+        else:
+            super().__init__(args[0], args[1])
+            self.value = args[2]
     def __str__(self):
-        return f"@{self.type_}/{self.name}"
+        return f"{self.type_}/{self.name}"
     def __repr__(self):
-        return f"ResourceTarget(resid={self.__str__()}, value={self.value})"
+        return f"ResourceTarget(\"{self.__str__()}\", \"{self.value}\")"
 
 class Overlay:
     def __init__(self, targetPkg: str, targetName: str, pkgName: str, static: bool, priority: int):
@@ -37,6 +44,19 @@ class Overlay:
         self.static = static
         self.priority = priority
         self.overlays = {}
-    
+
     def link_resource(self, target: ResourceTarget, res: Resource):
         self.overlays[target] = res
+
+    def build_linux(self, folder: str | None = None):
+        root = folder if folder else f"{self.targetPkg}-{self.targetName}-overlay"
+        shutil.rmtree(root, ignore_errors=True)
+        os.mkdir(root)
+        manifest = root + "/AndroidManifest.xml"
+        #with open(manifest, "x"): pass
+        with open(manifest, "w") as f:
+            f.write(f"<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+            f.write(f"<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\" package=\"{self.pkgName}\">\n")
+            f.write(f"    <application android:hasCode=\"false\" />\n")
+            f.write(f"    <overlay android:targetPackage=\"{self.targetPkg}\" android:targetName=\"{self.targetName}\" android:resourcesMap=\"@xml/overlays\" android:isStatic=\"{"true" if self.static else "false"}\" android:priority=\"{self.priority}\" />\n")
+            f.write(f"</manifest>\n")
