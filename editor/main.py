6e1a1a0023b7f19be732da0083f0899bf77d16de
mgismissing <gabrycraft2010@gmail.com>
2025-07-11 14:43:57 +0200

    Push

--- editor/main.py
+++
@@ -1,7 +0,0 @@
-import phosphor as ph
-
-overlay = ph.Overlay("com.android.systemui", "PhosphorSystemUI", "com.morph3us.ph0sphor.ui", True, 999)
-
-overlay.link_resource(ph.ResourceTarget("string", "sec_quick_settings_flashlight_label"), ph.Resource("string", "qs_flashlight", "Flashbang"))
-
-overlay.build_linux()
